2c44c5c60fe0d9f549dfe653dfb9f7494713cd7d
Vioreed <dsaauser6@gmail.com>
2024-01-11 22:57:19 +0700

    feat: dynamis content latter

--- src/app.service.ts
+++ src/app.service.ts
@@ -26,22 +26,28 @@ export class AppService {
       linebreaks: true,
     });
 
+    const date = new Date().toLocaleDateString('id-ID', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+    });
     doc.render({
       //buatkan format date dengan format tanggal bulan tahun, bulannya cukup 3 huruf, dalam bahasa indonesia bulannya
       //contoh: 01 Jan 2020
-      date: new Date().toLocaleDateString('id-ID', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-      }),
+      date: date,
       // no surat dengan random 3 angka
-      no_surat: Math.floor(Math.random() * 1000),
+      no_surat: 'YDDS/' + 'II/' + 'D' + Math.floor(Math.random() * 1000),
+      latter_province: 'Jawa Timur',
+      greeting: 'Kepada Yth.',
       name: 'John Doe',
       street: '1234 Main Street',
       rt_rw: '001/001',
       address: 'Sumampan, Kec. Sidoarjo, Kab. Sidoarjo',
-      province: 'East Java',
+      province: 'Jawa Timur',
       poscode: '61257',
+      short_desc: 'belum dilengkapi dengan kuitansi asli dari Rumah Sakit',
+      long_desc:
+        'dilengkapi dengan kuitansi asli dari Rumah Sakit, klaim ulang (untuk melengkapi berkas) diterima YDDS selambat - lambatnya 60 hari sejak tanggal pemberitahuan/surat YDDS',
     });
 
     const buf = doc
